b54b05b0a84e67c7c59bac34ab68dcb74e796a31
erik <erikpoljsak1@gmail.com>
2022-05-21 23:40:58 +0200

    Added files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { blue, green, grey, purple, red } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Box } from "@mui/system";
+import { deepmerge } from "@mui/utils";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import MenuBar from "./components/MenuBar";
+import { UserProvider } from "./components/UserContext";
+import Index from "./pages/Index";
+import PlayerStats from "./pages/PlayerStats";
+import PlayGrid from "./pages/PlayGrid";
 
 function App() {
+  const theme = createTheme(
+    deepmerge({
+      components: {
+        MuiButton: {
+          variants: [
+            {
+              props: { variant: "flat" },
+              style: {
+                color: "white",
+              },
+            },
+          ],
+        },
+      },
+      palette: {
+        mode: "dark",
+        primary: {
+          main: purple[500],
+          dark: purple[900],
+        },
+        secondary: {
+          main: green[500],
+          dark: green[900],
+        },
+        background: {
+          primary: grey[900],
+        },
+        blue: {
+          main: blue[500],
+          dark: blue[900],
+        },
+        red: {
+          main: red[500],
+          dark: red[900],
+        },
+      },
+      typography: {
+        fontFamily: '"Poppins", "Helvetica", "Arial", sans-serif',
+      },
+    })
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <Box>
+          <MenuBar />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="playgrid" element={<PlayGrid />} />
+            <Route path="stats" element={<PlayerStats />} />
+          </Routes>
+        </Box>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
